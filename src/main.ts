254c2350676e579e76b8fcc893eaa0e1a9a49f2a
Dmitry Sorokin <dmesorokin@greenatom.ru>
2023-04-06 16:21:16 +0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import { createBitAccessor, createBitGetter } from "./tasks/task1";
+import { createBitAccessor, createBitGetter } from "tasks/task1";
 
 const bitGetter = createBitGetter(new Uint8Array([0b1110, 0b1101]));
 const bitAccessor = createBitAccessor(new Uint8Array([0b1110, 0b1101]));
-
 
 // // Второй параметр это порядок бита "справа-налево"
 console.log(bitGetter.get(0, 1)); // 1
 console.log(bitGetter.get(1, 1)); // 0
 
-
 // // Второй параметр это порядок бита "справа-налево"
 bitAccessor.set(1, 1, 0); //
-console.log(bitAccessor.get(1, 1));    // 0
+console.log(bitAccessor.get(1, 1)); // 0
