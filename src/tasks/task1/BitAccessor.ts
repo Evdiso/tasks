65f0230c1ec614e73853b185f419c1ec8828dc3e
Dmitry Sorokin <dmesorokin@greenatom.ru>
2023-04-06 16:23:57 +0400

    update

--- src/tasks/task1/BitAccessor.ts
+++ src/tasks/task1/BitAccessor.ts
@@ -1,24 +1,28 @@
-import { BitGetter } from './BitGetter';
-import { UintArrayBit } from './types';
+import { BitGetter } from "./BitGetter";
+import { UintArrayBit } from "./types";
 
 export class BitAccessor extends BitGetter {
-    constructor(unitArray: Uint8Array) {
-        super(unitArray);
-    }
+  constructor(unitArray: Uint8Array) {
+    super(unitArray);
+  }
 
-    set(elem: number, bit: number, value: UintArrayBit): void {
-        if (!this.checkInputData({elem, bit, size: this.elemSize}) && !this.checkSetBitData(value)) return;
+  set(elem: number, bit: number, value: UintArrayBit): void {
+    if (
+      !this.checkInputData({ elem, bit, size: this.elemSize }) &&
+      !this.checkSetBitData(value)
+    )
+      return;
 
-        if (value === 1) {
-            this.buffer[elem] = this.buffer[elem] | (0b1 << bit);
-        } else {
-            this.buffer[elem] = this.buffer[elem] & ~(0b1 << bit);
-        }
+    if (value === 1) {
+      this.buffer[elem] = this.buffer[elem] | (1 << bit);
+    } else {
+      this.buffer[elem] = this.buffer[elem] & ~(1 << bit);
     }
+  }
 
-    checkSetBitData(value: UintArrayBit): boolean {
-        const isValidData = value === 0 || value === 1;
-        if (!isValidData) console.error('Wrong value data');
-        return isValidData;
-    }
+  checkSetBitData(value: UintArrayBit): boolean {
+    const isValidData = value === 0 || value === 1;
+    if (!isValidData) console.error("Wrong value data");
+    return isValidData;
+  }
 }
